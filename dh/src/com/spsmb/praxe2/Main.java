aee6cd23ccf1f4b242bb0fcd87f803eb89e9dc4d
tadeas.karban <tadeaskarab59@gmail.com>
2021-09-21 14:56:35 +0200

    zajda uz dal klik

--- dh/src/com/spsmb/praxe2/Main.java
+++ dh/src/com/spsmb/praxe2/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("Ahoj svete ");
         System.out.println("Zelena je trava, zajda neda klik");
         System.out.println("dalsi zmena");
+        System.out.println("Zajda ten klik udelal");
+
+
     }
 }
