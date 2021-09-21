1d88a555fe5fa5bea983e1d3f9964d0e91aa0bd0
tadeas.karban <tadeaskarab59@gmail.com>
2021-09-21 14:38:13 +0200

    zajda neda klik

--- dh/src/com/spsmb/praxe2/Main.java
+++ dh/src/com/spsmb/praxe2/Main.java
@@ -1,2 +1,8 @@
-package com.spsmb.praxe2;public class Main {
+package com.spsmb.praxe2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ahoj svete ");
+        System.out.println("Zelena je trava, zajda neda klik");
+    }
 }
